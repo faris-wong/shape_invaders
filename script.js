8bcacf9f76b66a59bfbf145af0dd5c9b67030c53
faris-wong <167754808+faris-wong@users.noreply.github.com>
2024-05-28 09:39:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function rng(chance) {
+  const roll = Math.floor(Math.random() * 100);
+  if (roll <= chance) {
+    console.log(`${roll}` + ` true`);
+    //return active === true;
+  } else {
+    console.log(`${roll}` + ` false`);
+    //return active === false
+  }
+}
+
+//const arrowkey = document.addEventListener("keypress", movement());
+
+function movement(currentPos, event){
+
+
+}
+
+const him = document.
+const position1 = document.getElementsByClassName('position-1')
+const position2 = document.getElementsByClassName('position-2')
